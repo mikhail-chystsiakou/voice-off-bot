e6a629e44eae8b2d9f4155ee49ca58c59f42c977
Volha Lytkina <volha.lytkina@netcracker.com>
2023-04-10 23:37:36 +0300

    fix message

--- src/main/java/org/example/Constants.java
+++ src/main/java/org/example/Constants.java
@@ -4,7 +4,7 @@ public class Constants
 {
     public static final String YES = "Yes";
     public static final String No = "No";
-    public static final String YOU_WAS_ADDED_TO_THE_SYSTEM = "You was added to the system. Subscribe to other person by clicking the button 'Follow user'.";
+    public static final String YOU_WAS_ADDED_TO_THE_SYSTEM = "You was added to the system. Subscribe to other person by clicking the button 'Subscribe to'.";
     public static final String YOU_HAVE_ALREADY_REGISTERED = "You have already registered";
 
 
