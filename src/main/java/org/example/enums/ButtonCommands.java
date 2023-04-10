6a626ed911cedf99dbdd51d946eeb197ef9e6e6c
Volha Lytkina <volha.lytkina@netcracker.com>
2023-04-10 21:48:44 +0300

    change text on button

--- src/main/java/org/example/enums/ButtonCommands.java
+++ src/main/java/org/example/enums/ButtonCommands.java
@@ -4,9 +4,9 @@ public enum ButtonCommands
 {
     PULL("/pull", "Get updates"),
     MANAGE_SUBSCRIPTIONS("/subscriptions", "Manage Subscriptions"),
-    REMOVE_SUBSCRIBER("/removefollower", "Unsubscribe user"),
+    REMOVE_SUBSCRIBER("/removefollower", "Remove follower"),
     UNSUBSCRIBE("/unsubscribe", "Unfollow user"),
-    SUBSCRIBE("/subscribe", "Subscribe to user"),
+    SUBSCRIBE("/subscribe", "Follow user"),
     REMOVE_RECORDING("/removerecording", "Remove this recording"),
     RETURN_TO_MAIN_MENU("/return", "Return to main menu");
 
